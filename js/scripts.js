4aab54813a77071719daab8b66b368341505789f
Yoseb Julian Ospitia Gomez <caleb2515@outlook.es>
2021-10-28 01:46:38 -0500

    3.0 formularios y detalles
    Se finalizó la funcionalidad de los aplicativos y se completó el boseto de los formularios

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,45 @@
 function sum(){
     let num1 = document.forCal.calNum1.value;
     let num2 = document.forCal.calNum2.value;
+    let calRes = document.forCal.calRes.value;
+    calRes = parseFloat(num1)+parseFloat(num2);
+    document.forCal.calRes.value = calRes;
 }
 
+function res(){
+    let num1 = document.forCal.calNum1.value;
+    let num2 = document.forCal.calNum2.value;
+    let calRes = document.forCal.calRes.value;
+    calRes = parseFloat(num1)-parseFloat(num2);
+    document.forCal.calRes.value = calRes;
+}
+function mul(){
+    let num1 = document.forCal.calNum1.value;
+    let num2 = document.forCal.calNum2.value;
+    let calRes = document.forCal.calRes.value;
+    calRes = parseFloat(num1)*parseFloat(num2);
+    document.forCal.calRes.value = calRes;
+}
+function div(){
+    let num1 = document.forCal.calNum1.value;
+    let num2 = document.forCal.calNum2.value;
+    let calRes = document.forCal.calRes.value;
+    calRes = parseFloat(num1)/parseFloat(num2);
+    document.forCal.calRes.value = calRes;
+}
+function por(){
+    let num1 = document.forCal.calNum1.value;
+    let num2 = document.forCal.calNum2.value;
+    let calRes = document.forCal.calRes.value;
+    calRes = parseFloat(num1)*(parseFloat(num2)/100);
+    document.forCal.calRes.value = calRes;
+}
 // CONVERSOR APP
 function calConversor(){
     let numEva = document.forCon.conNum.value;
     let dat1 = document.forCon.tipDat1.value;
     let dat2 = document.forCon.tipDat2.value;
-    let result = document.forCon.conRes.value;
-    result = (parseFloat(numEva)*parseFloat(dat2))/parseFloat(dat1);
-    document.forCon.conRes.value = result;
+    let conRes = document.forCon.conRes.value;
+    conRes = (parseFloat(numEva)*parseFloat(dat2))/parseFloat(dat1);
+    document.forCon.conRes.value = conRes;
 }
